ec7ccc0f62dd35f5675f0d5ac95079069551c71c
sochoac1 <sochoac1@eafit.edu.co>
2024-03-17 21:44:38 -0500

    feat : creating api

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # PythonCICD
+**Instalar dependencias proyecto**
+`pip install -r requirements.txt`
+**Correr el proyecto**
+`uvicorn functions.crud_function:app --host 0.0.0.0 --port 3006 --reload`
---
+++ functions/crud_function.py
@@ -0,0 +1,24 @@
+from main import app
+from typing import List
+from entities.models.model import Item
+
+items = [{"name": "santi", "id": 1, "email": "santii@gmail.com"}]
+
+@app.get("/items", response_model=List[Item])
+async def read_item():
+    return items
+
+@app.post("/items", response_model=Item)
+async def create_item(item: Item):
+    items.append(item)
+    return item
+
+@app.put("/items/{item_id}", response_model=Item)
+async def update_item(item_id: int, item: Item):
+    items[item_id] = item
+    return item
+
+@app.delete("/items/{item_ud}")
+async def delete_item(item_id: int):
+    del items[item_id]
+    return {"message" : "Item deleted"}
